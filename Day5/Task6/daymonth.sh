e81d8cd070e18752db04d8b03f3c0c87b628a524
Harshita Suresh Chandaliya <77772422+Harshu3@users.noreply.github.com>
2023-03-22 19:28:05 +0530

    Update daymonth.sh

--- Day5/Task6/daymonth.sh
+++ Day5/Task6/daymonth.sh
@@ -1,3 +1,5 @@
+#Program that takes day and month from command line and print true if value is between March 20 and June 20, else false.
+
 date1="0319" #0320
 date2="0621" #0620
 echo "Please enter the date in mmdd format: "
